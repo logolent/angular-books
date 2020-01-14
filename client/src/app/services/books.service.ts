64b9ff3ddb6845265cae7a98d5ca5bbcd8e627e5
Logolent <email>
2020-01-14 21:55:05 +0400

    proxy

--- client/src/app/services/books.service.ts
+++ client/src/app/services/books.service.ts
@@ -13,7 +13,7 @@ export class BooksService {
 	}
 
 	public getBooks(search: string = ''): Observable<BookModel[]> {
-		return this.http.get<any>('http://localhost:3000/api/books').pipe(
+		return this.http.get<any>('/api/books').pipe(
 			map(array => array.data.filter((object: BookModel) =>
 				object.name.toLowerCase().includes(search.toLowerCase()) ||
 				object.author.toLowerCase().includes(search.toLowerCase())
@@ -22,20 +22,20 @@ export class BooksService {
 	}
 
 	public getBook(id: string): Observable<BookModel> {
-		return this.http.get<any>('http://localhost:3000/api/books/' + id).pipe(
+		return this.http.get<any>('/api/books/' + id).pipe(
 			map(object => object.data)
 		);
 	}
 
 	public addBook(body: BookModel): Observable<void> {
-		return this.http.post<any>('http://localhost:3000/api/books/add/', body);
+		return this.http.post<any>('/api/books/add/', body);
 	}
 
 	public deleteBook(id: string): Observable<void> {
-		return this.http.delete<any>('http://localhost:3000/api/books/delete/' + id);
+		return this.http.delete<any>('/api/books/delete/' + id);
 	}
 
 	public updateBook(id: string, body: object): Observable<void> {
-		return this.http.patch<any>('http://localhost:3000/api/books/edit/' + id, body);
+		return this.http.patch<any>('/api/books/edit/' + id, body);
 	}
 }
