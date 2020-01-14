88ca8f99d0d908c91c79796f68f130e8d30f63ce
Logolent <email>
2020-01-15 01:37:40 +0400

    deleted proxy

--- client/src/app/services/books.service.ts
+++ client/src/app/services/books.service.ts
@@ -3,17 +3,20 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class BooksService {
-	constructor(private http: HttpClient) {
 
+	apiUrl = environment.apiUrl;
+
+	constructor(private http: HttpClient) {
 	}
 
 	public getBooks(search: string = ''): Observable<BookModel[]> {
-		return this.http.get<any>('/api/books').pipe(
+		return this.http.get<any>(this.apiUrl + '/api/books').pipe(
 			map(array => array.data.filter((object: BookModel) =>
 				object.name.toLowerCase().includes(search.toLowerCase()) ||
 				object.author.toLowerCase().includes(search.toLowerCase())
@@ -22,20 +25,20 @@ export class BooksService {
 	}
 
 	public getBook(id: string): Observable<BookModel> {
-		return this.http.get<any>('/api/books/' + id).pipe(
+		return this.http.get<any>(this.apiUrl + '/api/books/' + id).pipe(
 			map(object => object.data)
 		);
 	}
 
 	public addBook(body: BookModel): Observable<void> {
-		return this.http.post<any>('/api/books/add/', body);
+		return this.http.post<any>(this.apiUrl + '/api/books/add/', body);
 	}
 
 	public deleteBook(id: string): Observable<void> {
-		return this.http.delete<any>('/api/books/delete/' + id);
+		return this.http.delete<any>(this.apiUrl + '/api/books/delete/' + id);
 	}
 
 	public updateBook(id: string, body: object): Observable<void> {
-		return this.http.patch<any>('/api/books/edit/' + id, body);
+		return this.http.patch<any>(this.apiUrl + '/api/books/edit/' + id, body);
 	}
 }
